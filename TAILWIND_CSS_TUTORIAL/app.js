a5de3742ab8dc049e6cc6cae3c65519f8601abb5
Jaimin Mukesh Jariwala <jaiminjariwala5@gmail.com>
2024-12-09 21:28:53 +0530

    updated js logic for hamburger menu

--- TAILWIND_CSS_TUTORIAL/app.js
+++ TAILWIND_CSS_TUTORIAL/app.js
@@ -1,15 +1,13 @@
 const hamburgerDiv = document.querySelector("#hamburger-div");
-const hamburgerLines = document.querySelectorAll(".hamburger-line");
 const menu = document.querySelector("#menu");
 
 hamburgerDiv.addEventListener("click", () => {
-  // console.log(hamburgerLines)
   menu.classList.toggle("hidden");
-  hamburgerLines.forEach((line) => {
-    if (menu.classList.contains("hidden")) {
-      // set lines back to black when menu is hidden
-      line.classList.remove("bg-white");
-      line.classList.add("bg-black");
-    }
-  });
+  
+  // toggle background color for the #hamburger-div
+  if (menu.classList.contains("hidden")) {
+    hamburgerDiv.style.backgroundColor = "white";
+  } else {
+    hamburgerDiv.style.backgroundColor = "#EEEEEE";
+  }
 });
